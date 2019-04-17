98593a750c6205f1410bfcdb837a11587548c2f6
liuzhijie <meajhlp@gmail.com>
2019-04-17 16:59:32 +0800

    [node] 解决本地重写host时浏览器控制台报错问题

--- node/examples/frontend_scaffold/vue_muse_ui/build/webpack.dev.conf.js
+++ node/examples/frontend_scaffold/vue_muse_ui/build/webpack.dev.conf.js
@@ -22,6 +22,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
 
   // these devServer options should be customized in /config/index.js
   devServer: {
+    disableHostCheck: true, // fixed bug when host rewrite in local
     clientLogLevel: 'warning',
     historyApiFallback: {
       rewrites: [
